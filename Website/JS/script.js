0f2c771b0c9fc2a06c6b27d5a3935fa4dc174158
rutu728 <palekarrutuja4>
2022-03-19 12:59:47 +0530

    Updated

---
+++ Website/JS/script.js
@@ -0,0 +1,10 @@
+function getData(){
+    const name = document.getElementById("name").value;
+    const Email = document.getElementById("email").value;
+    const Mobile  = document.getElementById("mobileno").value;
+    const College = document.getElementById("College").value;
+    alert(`Name : ${name}
+Email : ${Email}
+Mobile Number : ${Mobile}
+College : ${College}`);
+}
